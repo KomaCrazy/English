d73cc50cf49cbfd4dfd153daff7f5348c40e9af4
koma <zxloveninek90@gmail.com>
2024-07-24 23:31:13 +0700

    add middleware

--- api/src/app.js
+++ api/src/app.js
@@ -3,15 +3,18 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-
-
 const indexRoutes = require('./routes/index');
 const adminRoutes = require('./routes/admin');
 
+app.use((req, res, next) => {
+	console.log('Time: ', Date.now())
+	next()
+})
 
 app.use('/',indexRoutes)
 app.use('/admin',adminRoutes)
 
+app.get
 
 app.listen(port, () => {
 	console.log(`Server running at <http://localhost>:${port}/`);
